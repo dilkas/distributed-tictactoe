3b8d174b9b61d5293ffabc0c0734ac8a1ed144ea
ArcStatic <eband42@gmail.com>
2017-12-01 15:36:15 +0000

    completed performance testing and write-up

--- createpeers.py
+++ createpeers.py
@@ -6,7 +6,11 @@ import threading
 
 def client_thread(i, move):
     #Create client, execute move on repeat
-    subprocess.call("gnome-terminal -x sh -c \"java Game client%d server localhost; sleep 5; while true; do %d; done\"" % (i, move), shell=True)
+    subprocess.call("gnome-terminal -x sh -c \"yes %d | java Game client%d server localhost\"" % (i, move), shell=True)
+    
+def client_thread_no_move(i):
+    #Create client, execute move on repeat
+    subprocess.call("gnome-terminal -x sh -c \"java Game client%d server localhost\"" % i, shell=True)
     
 
 if sys.argv[1] == "server":
@@ -29,10 +33,27 @@ elif sys.argv[1] == "clients":
             c_thread.start()
             #subprocess.call("gnome-terminal -x sh -c \"java Game client%d server localhost\"" % i, shell=True)
             #sleep to allow time for server to resolve connections
-            time.sleep(5)
+            time.sleep(1)
+        print("%d peers created" % peer_count)
+    except Exception as e:
+        print(e)
+        
+elif sys.argv[1] == "clients_no_move":
+    try:
+        #number of peers to create in the session
+        peer_count = int(sys.argv[2])
+        for i in range(peer_count):
+            #create instance threads named client0, client1, etc. in a new window
+            print("Creating peer thread %d" % i)
+            move = random.randint(0,8)
+            c_thread = threading.Thread(target=client_thread_no_move, args=(i,))
+            c_thread.start()
+            #subprocess.call("gnome-terminal -x sh -c \"java Game client%d server localhost\"" % i, shell=True)
+            #sleep to allow time for server to resolve connections
+            time.sleep(1)
         print("%d peers created" % peer_count)
     except Exception as e:
         print(e)
 
 else:
-    print("Usage:\npython server - creates base server instance on localhost\npython clients [number] creates [number] of clients to connect to server instance")
+    print("Usage:\npython server - creates base server instance on localhost\npython clients [number] - creates [number] of clients to connect to server instance\npython clients_no_move [number] - creates [number] of clients to connect to server instance without making any moves")
