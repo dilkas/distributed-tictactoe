e39cd0b541a54bb4f671954f7e6eb45e561656ae
ArcStatic <eband42@gmail.com>
2017-12-01 12:09:05 +0000

    updated peer creation script

--- createpeers.py
+++ createpeers.py
@@ -1,11 +1,16 @@
 import sys
 import subprocess
 import time
+import random
+import threading
+
+def client_thread(i, move):
+    #Create client, execute move on repeat
+    subprocess.call("gnome-terminal -x sh -c \"java Game client%d server localhost; sleep 5; while true; do %d; done\"" % (i, move), shell=True)
+    
 
 if sys.argv[1] == "server":
     try:
-        #run rmiregistry in the background
-        subprocess.call("rmiregistry &", shell=True)
         #create base server instance
         subprocess.call("java Game server client localhost", shell=True)
         print("Server created")
@@ -17,9 +22,12 @@ elif sys.argv[1] == "clients":
         #number of peers to create in the session
         peer_count = int(sys.argv[2])
         for i in range(peer_count):
-            #create instances named client0, client1, etc. in a new window
-            print("Creating peer %d" % i)
-            subprocess.call("gnome-terminal -x sh -c \"java Game client%d server localhost\"" % i, shell=True)
+            #create instance threads named client0, client1, etc. in a new window
+            print("Creating peer thread %d" % i)
+            move = random.randint(0,8)
+            c_thread = threading.Thread(target=client_thread, args=(i, move))
+            c_thread.start()
+            #subprocess.call("gnome-terminal -x sh -c \"java Game client%d server localhost\"" % i, shell=True)
             #sleep to allow time for server to resolve connections
             time.sleep(5)
         print("%d peers created" % peer_count)
@@ -27,4 +35,4 @@ elif sys.argv[1] == "clients":
         print(e)
 
 else:
-    print("Usage:\npython server - creates base server instance on localhost\npython clients [number] - creates [number] of clients to connect to server instance")
+    print("Usage:\npython server - creates base server instance on localhost\npython clients [number] creates [number] of clients to connect to server instance")
